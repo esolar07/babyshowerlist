9c07971ded9d1d6c3cfde8dc6bb085943ae54b57
esolar07 <esolar07@gmail.com>
2016-12-08 17:31:43 -0500

    making style changes. working on sql count query for guests

--- guests.php
+++ guests.php
@@ -5,9 +5,11 @@
 	include ("inc/header.php");
 	$pdo = Database::connection();
 	$sql = "SELECT * FROM guest_list ORDER BY name ASC";
+	$sqlRsvps = "SELECT COUNT(name) FROM guest_list";
 	
 	try{
 		$guests = $pdo->query($sql);
+		$rsvps = $pdo->query($sqlRvsps);
 	} catch(Exception $e){
 		echo $e->getMessage();
 		die();
@@ -17,10 +19,11 @@
 ?>
 <div class="body-container">
 	<h2 class="body-title"> <?php echo ucwords($pageTitle); ?> </h2>
+	<div> <?php echo $rsvps . " have RSVP'd."; ?> </div>
 	<ul class="guest-list">
 		<?php foreach($guestList as $guest){ ?>
 			<li class="guest-list__person">
-				<?php echo "<div> Name: " . $guest["name"] . "</div>"; ?>
+				<?php echo "<div> <span class='guest-list__person-name'>Name:</span> " . $guest["name"] . "</div>"; ?>
 				<?php echo "<div> Guests: " . $guest["total_guest"] . "</div>"; ?>
 			</li>					
 		<?php } ?>
