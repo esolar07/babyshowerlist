10c94546828607fb5c510b24e4ecef4b6367f9b1
esolar07 <esolar07@gmail.com>
2016-12-09 15:03:03 -0500

    fixed issue with person count. made more style changes

--- guests.php
+++ guests.php
@@ -5,29 +5,38 @@
 	include ("inc/header.php");
 	$pdo = Database::connection();
 	$sql = "SELECT * FROM guest_list ORDER BY name ASC";
-	$sqlRsvps = "SELECT COUNT(name) FROM guest_list";
+	
 	
 	try{
 		$guests = $pdo->query($sql);
-		$rsvps = $pdo->query($sqlRvsps);
 	} catch(Exception $e){
 		echo $e->getMessage();
 		die();
 	}	
 	
 	$guestList = $guests -> fetchAll(PDO::FETCH_ASSOC);
+	$count = $guests -> rowCount();
+
 ?>
 <div class="body-container">
 	<h2 class="body-title"> <?php echo ucwords($pageTitle); ?> </h2>
-	<div> <?php echo $rsvps . " have RSVP'd."; ?> </div>
-	<ul class="guest-list">
-		<?php foreach($guestList as $guest){ ?>
-			<li class="guest-list__person">
-				<?php echo "<div> <span class='guest-list__person-name'>Name:</span> " . $guest["name"] . "</div>"; ?>
-				<?php echo "<div> Guests: " . $guest["total_guest"] . "</div>"; ?>
-			</li>					
-		<?php } ?>
-	</ul>
+	<div class="guest-info">
+		<div class="guest-info__box"> 
+			<p class="guest-info__count"><?php echo $count ; ?></p>
+			<p>have RSVP'd.</p>
+		</div>
+		<div class="guest-info__box guest-info__box--list">
+			<ul class="guest-list">
+				<?php foreach($guestList as $guest){ ?>
+					<li class="guest-list__person">
+						<?php echo "<div> <span class='guest-list__person-name'>Name:</span> " . $guest["name"] . "</div>"; ?>
+						<?php echo "<div> Guests: " . $guest["total_guest"] . "</div>"; ?>
+					</li>					
+				<?php } ?>
+			</ul>
+		</div>
+		<div class="guest-info__box"></div>
+	</div>
 </div>
 
 <?php include ("inc/footer.php");?>
