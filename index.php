2a2ba41a1d357e5730c8727df4c7b2e3ef4937a1
esolar07 <esolar07@gmail.com>
2016-12-12 17:30:24 -0500

    started making style changes to form

--- index.php
+++ index.php
@@ -6,21 +6,21 @@
 	<div class="body-container">
 		<h2 class="body-title"> <?php echo ucwords($pageTitle); ?> </h2>
 			<form class="rsvp-form" action="addToList.php" method="post">
-				<fieldset class="rsvp-form__set">
+				<fieldset class="rsvp-form__set rsvp-form__set--1">
 					<label class="rsvp-form__label" for="name">FullName</label>
 					<input class="rsvp-form__input" type="text" name="name" id="name" required>
 					<?php if (!empty($errorName)): ?>
 						<span class="rsvp__help"><?php echo $errorName;?></span>
 					<?php endif; ?>
 				</fieldset>
-				<fieldset class="rsvp-form__set">
+				<fieldset class="rsvp-form__set rsvp-form__set--2">
 					<label class="rsvp-form__label" for="guests">Guests</label>
 					<input class="rsvp-form__input" type="text"  name="guests" id="guests" required>
 					<?php if (!empty($errorGuest)): ?>
 						<span class="rsvp__help"><?php echo $errorGuest;?></span>
 					<?php endif; ?>
 				</fieldset>
-				<fieldset class="rsvp-form__set">
+				<fieldset class="rsvp-form__set rsvp-form__set--3">
 					<button class="rsvp-form__btn" type="submit">RSVP</button>
 				</fieldset>
 			</form>
