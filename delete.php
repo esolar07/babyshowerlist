c6689f43db3102a922dc7e69a2181d1d1ab6a399
esolar07 <esolar07@gmail.com>
2016-12-15 17:29:35 -0500

    fixing issue with guest delete feature

--- delete.php
+++ delete.php
@@ -3,16 +3,21 @@
 	$pageTitle = "delete";
 	include ("inc/header.php");
 	
+	$id = 0;
+	if (!empty($_GET)){
+		$id = $_REQUEST['id'];
+    }
 	
 	if (!empty($_POST)){
 		$id = $_POST['id'];
-		$sql = "Delete * FROM guest_list WHERE id = ?";
+		$sql = "DELETE FROM guest_list WHERE id = ?";
 		
 		try{
 			$pdo = Database::connection();
 			$deleteQuery = $pdo -> prepare($sql);
 			$deleteQuery -> bindParam($id);
 			$deleteQuery -> execute();
+			header("Location: /babyshower/guests.php");
 		} catch(Exception $e){
 			echo $e->getMessage();
 			die();
@@ -22,6 +27,12 @@
 ?>
 <div class="body-container">
 	
-	<h1>Are you sure you want to delete this guest.</h1>
-
+	<h1 class="body-title">Are you sure you want to delete this guest.</h1>
+	
+	<form class="delete-form" action="addToList.php" method="post">
+		<input type="hidden" name="id" value="<?php echo $id;?>"/>
+		<button type="submit" class="btn btn-delete">Yes</button>
+        <button class="btn btn-nodelete"><a class="btn" href="/babyshower/guests.php">No</a><button>
+	</form>
+	
 </div>
