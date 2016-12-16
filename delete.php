33608292bc11fcd1aa4676f2790f9f26bbe1c28b
esolar07 <esolar07@gmail.com>
2016-12-16 17:23:09 -0500

    fixed issue with deleteling guest from db. made some style changes.

--- delete.php
+++ delete.php
@@ -1,7 +1,4 @@
 <?php
-
-	$pageTitle = "delete";
-	include ("inc/header.php");
 	
 	$id = 0;
 	if (!empty($_GET)){
@@ -11,11 +8,11 @@
 	if (!empty($_POST)){
 		$id = $_POST['id'];
 		$sql = "DELETE FROM guest_list WHERE id = ?";
-		
+		require_once('db.php');
 		try{
 			$pdo = Database::connection();
 			$deleteQuery = $pdo -> prepare($sql);
-			$deleteQuery -> bindParam($id);
+			$deleteQuery -> bindParam(1,$id, PDO::PARAM_INT);
 			$deleteQuery -> execute();
 			header("Location: /babyshower/guests.php");
 		} catch(Exception $e){
@@ -24,12 +21,15 @@
 		}
 		
 	}
+	
+	$pageTitle = "delete";
+	include ("inc/header.php");
 ?>
 <div class="body-container">
 	
 	<h1 class="body-title">Are you sure you want to delete this guest.</h1>
 	
-	<form class="delete-form" action="addToList.php" method="post">
+	<form class="delete-form" action="delete.php" method="post">
 		<input type="hidden" name="id" value="<?php echo $id;?>"/>
 		<button type="submit" class="btn btn-delete">Yes</button>
         <button class="btn btn-nodelete"><a class="btn" href="/babyshower/guests.php">No</a><button>
