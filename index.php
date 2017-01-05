66922706e3fde1aa744128e960c9da51f80d6286
esolar07 <esolar07@gmail.com>
2017-01-05 17:22:11 -0500

    fixed issue with rotating form. made more css changes to mobile layout

--- index.php
+++ index.php
@@ -5,19 +5,19 @@
 ?>
 	<div class="body-container">
 		<h2 class="body-title"> <?php echo ucwords($pageTitle); ?> </h2>
-			<form class="form rsvp-form" id="js-form" action="addToList.php" method="post">
-				<div class="rsvp-form__set rsvp-form__set--1">
-					<input class="rsvp-form__input" type="text" name="name" id="name" placeholder="Full Name" required>
-					<input value="Next" class="rsvp-form__next" id="next--1" >
-				</div>
-				<div class="rsvp-form__set rsvp-form__set--2">
-					<input class="rsvp-form__input" type="text"  name="guests" id="guests" placeholder="# of Guests" required>
-					<input value="Next" class="rsvp-form__next" id="next--2" >
-				</div>
-				<div class="rsvp-form__set rsvp-form__set--3">
-					<button class="rsvp-form__btn" type="submit">RSVP</button>
-				</div>
-			</form>
+		<form class="form rsvp-form"  id="js-form"  action="addToList.php" method="post">
+			<div class="rsvp-form__set rsvp-form__set--1">
+				<input class="rsvp-form__input" type="text" name="name" id="name" placeholder="Full Name" required>
+				<button class="rsvp-form__next" id="next--1">Next</button>
+			</div>
+			<div class="rsvp-form__set rsvp-form__set--2">
+				<input class="rsvp-form__input" type="text"  name="guests" id="guests" placeholder="# of Guests" required>
+				<button class="rsvp-form__next" id="next--2">Next</button>
+			</div>
+			<div class="rsvp-form__set rsvp-form__set--3">
+				<button class="rsvp-form__btn" id="submit" type="submit">RSVP</button>
+			</div>
+		</form>
 	</div>
 			
 <?php include ("inc/footer.php");?>
--- js/main.js
+++ js/main.js
@@ -5,7 +5,8 @@
 		name = document.querySelector("#name"),
 		next1 = document.querySelector("#next--1"),
 		guests = document.querySelector("#guests"),
-		next2 = document.querySelector("#next--2");
+		next2 = document.querySelector("#next--2"),
+		submit = document.querySelector("#submit");
 		
 	// disabling form button until js validation
 	next1.disabled = true;
@@ -20,11 +21,11 @@
 		
 			if (typeof iptVal == "string" && typeof iptVal !== null){
 				console.log('passed');
+				ipt.focus();
 				ipt.style.backgroundColor = "lightgreen";
 				btn.disabled = false;
 				btn.style.backgroundColor = "#4682B4";
 				btn.addEventListener("click", function(){
-					console.log("clicking");
 					form.style.transform  = "rotateX("+ rot +"deg)";
 				 })
 			} 
@@ -33,9 +34,8 @@
 		
 	}
 	
-	
+
 	formIptCheck(name, next1, 90);
 	formIptCheck(guests, next2, 180);
-	
 	
 }())
